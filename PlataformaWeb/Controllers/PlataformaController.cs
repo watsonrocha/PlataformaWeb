65cffa6b3670db2ecc81282ff6d899d39ddcf5c3
Watson Rocha <watson.rocha@TKLBRASIL.LOCAL>
2018-04-23 07:36:57 -0300

    Adicionar arquivos de projeto.

---
+++ PlataformaWeb/Controllers/PlataformaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlataformaWeb.Controllers
+{
+    public class PlataformaController:Controller
+    {
+       public IActionResult Index()
+        {
+
+            return View();
+
+        }
+        
+    }
+}
